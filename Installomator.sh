20bb409f92290ed05dcf6719408c86268cb89af5
Vladyslav Reva <v.reva1997@gmail.com>
2024-04-23 14:05:07 +0300

    Add teamviewercustom label (TeamViewer custom full client)

--- Installomator.sh
+++ Installomator.sh
@@ -7317,6 +7317,16 @@ teamviewer)
     appNewVersion=$(curl -fs "https://www.teamviewer.com/en/download/macos/" | grep "Current version" | awk -F': ' '{ print $2 }' | sed 's/<[^>]*>//g')
     expectedTeamID="H7UGFBUGV6"
     ;;
+teamviewercustom)
+    name="TeamViewerHost"
+    type="pkg"
+    packageID="com.teamviewer.teamviewer"
+    teamviewerConfigID="6b3yjfa"
+    archiveName="TeamViewer-idc$teamviewerConfigID.pkg"
+    downloadURL="https://dl.teamviewer.com/download/version_15x/CustomDesign/Install%20TeamViewer-idc$teamviewerConfigID.pkg"
+    appNewVersion=$(curl -fs "https://www.teamviewer.com/en/download/macos/" | grep "Current version" | awk -F': ' '{ print $2 }' | sed 's/<[^>]*>//g')
+    expectedTeamID="H7UGFBUGV6"
+    ;;
 teamviewerhost)
     name="TeamViewerHost"
     type="pkgInDmg"
