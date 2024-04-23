d716b32b3a3cdaed68e9d7e8d4757623940337ec
Vladyslav Reva <v.reva1997@gmail.com>
2024-04-23 11:03:25 +0300

    Fully fix teamviewerhostcustom label

--- fragments/labels/teamviewerhostcustom.sh
+++ fragments/labels/teamviewerhostcustom.sh
@@ -1,10 +1,10 @@
 teamviewerhostcustom)
     name="TeamViewerHost"
     type="pkg"
+    packageID="com.teamviewer.teamviewerhost"
     teamviewerConfigID="6f3jm76"
     archiveName="TeamViewerHost-idc$teamviewerConfigID.pkg"
     downloadURL="https://dl.teamviewer.com/download/version_15x/CustomDesign/Install%20TeamViewerHost-idc$teamviewerConfigID.pkg"
     appNewVersion=$(curl -fs "https://www.teamviewer.com/en/download/macos/" | grep "Current version" | awk -F': ' '{ print $2 }' | sed 's/<[^>]*>//g')
-    appName="TeamViewer.app"
     expectedTeamID="H7UGFBUGV6"
     ;;
