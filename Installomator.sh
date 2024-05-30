8c901c58b638742023c33ceef9a57686313fa3b2
Vladyslav Reva <v.reva1997@gmail.com>
2024-05-30 18:14:03 +0300

    Change label view (4)

--- Installomator.sh
+++ Installomator.sh
@@ -3685,8 +3685,12 @@ googleadseditor)
 googlechromepkg)
     name="Google Chrome"
     type="pkg"
-    downloadURL=""
-    appNewVersion=$( curl -s -X GET "${mdmURL%/}/JSSResource/computergroups/id/$jamfGroupID" -H "accept: application/xml" -H "Authorization: Bearer $jamfBearerToken" | xmllint --xpath '/computer_group/criteria/criterion[priority="2"]/value/text()' - )
+    #
+    # Note: this url acknowledges that you accept the terms of service
+    # https://support.google.com/chrome/a/answer/9915669
+    #
+    downloadURL="https://dl.google.com/chrome/mac/stable/accept_tos%3Dhttps%253A%252F%252Fwww.google.com%252Fintl%252Fen_ph%252Fchrome%252Fterms%252F%26_and_accept_tos%3Dhttps%253A%252F%252Fpolicies.google.com%252Fterms/googlechrome.pkg"
+    appNewVersion=$(curl -s -X GET "${mdmURL%/}/JSSResource/computergroups/id/$jamfGroupID" -H "accept: application/xml" -H "Authorization: Bearer $jamfBearerToken" | xmllint --xpath '/computer_group/criteria/criterion[priority="2"]/value/text()' -)
     expectedTeamID="EQHXZ8M8AV"
     jamfPolicyEvent="update_chrome_test"
     jamfDownload=true
