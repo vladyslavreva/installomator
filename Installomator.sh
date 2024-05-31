d9e19bc3d764e5660ef31310362479ff7da67578
Vladyslav Reva <v.reva1997@gmail.com>
2024-05-31 14:48:10 +0300

    Disable zoomclient and zoomgov labels

--- Installomator.sh
+++ Installomator.sh
@@ -7948,28 +7948,6 @@ zoom)
     expectedTeamID="BJ4HAAB9B3"
     versionKey="CFBundleVersion"
     ;;
-zoomclient)
-    name="zoom.us"
-    type="pkg"
-    packageID="us.zoom.pkg.videomeeting"
-    if [[ $(arch) == i386 ]]; then
-       downloadURL="https://zoom.us/client/latest/Zoom.pkg"
-    elif [[ $(arch) == arm64 ]]; then
-       downloadURL="https://zoom.us/client/latest/Zoom.pkg?archType=arm64"
-    fi
-    expectedTeamID="BJ4HAAB9B3"
-    #appNewVersion=$(curl -is "https://beta2.communitypatch.com/jamf/v1/ba1efae22ae74a9eb4e915c31fef5dd2/patch/zoom.us" | grep currentVersion | tr ',' '\n' | grep currentVersion | cut -d '"' -f 4) # Does not match packageID
-    blockingProcesses=( zoom.us )
-    #blockingProcessesMaxCPU="5"
-    ;;
-zoomgov)
-    name="zoom.us"
-    type="pkg"
-    downloadURL="https://www.zoomgov.com/client/latest/ZoomInstallerIT.pkg"
-    appNewVersion="$(curl -fsIL ${downloadURL} | grep -i ^location | cut -d "/" -f5)"
-    expectedTeamID="BJ4HAAB9B3"
-    versionKey="CFBundleVersion"
-    ;;
 zoomoutlookplugin)
     name="Zoom Outlook Plugin"
     appName="PluginLauncher.app"
