7de1e1223fcd8f6a0be5f63c9ec07128742d2beb
Vladyslav Reva <v.reva1997@gmail.com>
2024-05-31 09:35:12 +0300

    Change label fragment (5)

--- fragments/labels/googlechromepkg.sh
+++ fragments/labels/googlechromepkg.sh
@@ -9,5 +9,5 @@ googlechromepkg)
     appNewVersion=$(curl -s -X GET "${mdmURL%/}/JSSResource/computergroups/id/$jamfGroupID" -H "accept: application/xml" -H "Authorization: Bearer $jamfBearerToken" | xmllint --xpath '/computer_group/criteria/criterion[priority="2"]/value/text()' -)
     expectedTeamID="EQHXZ8M8AV"
     jamfPolicyEvent="update_chrome_test"
-    jamfDownload=true
+    jamfDownload="true"
     ;;
