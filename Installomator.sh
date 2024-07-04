146121afc9d70d01e59541215584220627fb3764
Vladyslav Reva <v.reva1997@gmail.com>
2024-07-04 09:20:41 +0300

    Add arm64 version for virtualbox label

--- Installomator.sh
+++ Installomator.sh
@@ -7548,8 +7548,13 @@ virtualbox)
     name="VirtualBox"
     type="pkgInDmg"
     pkgName="VirtualBox.pkg"
-    downloadURL="https:$(curl -fsL "https://www.oracle.com/virtualization/technologies/vm/downloads/virtualbox-downloads.html" | grep "OSX.dmg" | xmllint --html --xpath 'string(//a/@href)' -)"
-    appNewVersion=$(echo "${downloadURL}" | awk -F '/' '{print $5}')
+    if [[ $(arch) == "arm64" ]]; then
+        downloadURL=$(curl -fsL "https://www.virtualbox.org/wiki/Testbuilds" | grep "macOSArm64.dmg" | xmllint --html --xpath 'string(//a/@href)' -)
+        appNewVersion=$(echo "$downloadURL" | awk -F'[-_]' '{print $2}')
+    elif [[ $(arch) == "i386" ]]; then
+        downloadURL="https:$(curl -fsL "https://www.oracle.com/virtualization/technologies/vm/downloads/virtualbox-downloads.html" | grep "OSX.dmg" | xmllint --html --xpath 'string(//a/@href)' -)"
+        appNewVersion=$(echo "${downloadURL}" | awk -F '/' '{print $5}')
+    fi
     expectedTeamID="VB5E2TV963"
     ;;
 virtualbuddy)
