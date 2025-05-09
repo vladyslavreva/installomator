a7a1efff2086e11afc28b0faec3d6479090b902d
Vladyslav Reva <v.reva1997@gmail.com>
2025-05-09 17:38:13 +0300

    Fix whatsapp label

--- Installomator.sh
+++ Installomator.sh
@@ -7763,8 +7763,8 @@ whatsapp)
     name="WhatsApp"
     type="dmg"
     downloadURL="https://web.whatsapp.com/desktop/mac_native/release/?configuration=Release"
+    appNewVersion=$(curl -fsIL "https://web.whatsapp.com/desktop/mac_native/release/?configuration=Release" | grep -i "^location" | grep -m 1 -o "WhatsApp-.*dmg" | sed 's/.*WhatsApp-2.//g' | sed 's/.dmg//g')
     expectedTeamID="57T9237FN3"
-    appNewVersion=$(curl -s https://web.whatsapp.com/desktop/mac/releases | awk -F '"' '/"name"/ {print $4}')
     ;;
 wireshark)
     name="Wireshark"
