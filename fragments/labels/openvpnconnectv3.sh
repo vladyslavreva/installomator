41713c0879170dbdf3045937080027555643fad4
Vladyslav Reva <v.reva1997@gmail.com>
2025-05-13 09:44:14 +0300

    Fix openvpnconnectv3 label

--- fragments/labels/openvpnconnectv3.sh
+++ fragments/labels/openvpnconnectv3.sh
@@ -7,8 +7,8 @@ openvpnconnectv3)
     elif [[ $(arch) == "i386" ]]; then
         pkgName="OpenVPN_Connect_[0-9_()]*_x86_64_Installer_signed.pkg"
     fi
-    appNewVersion=$(curl -fs "https://openvpn.net/connect-docs/macos-release-notes.html" | awk '/<aside class="section-nav-container">/,/<\/aside>/' | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' | head -n 1)
     downloadURL="https://openvpn.net/downloads/openvpn-connect-v3-macos.dmg"
+    appNewVersion=$(curl -fsIL "$downloadURL" | sed -nE 's/.*openvpn-connect-([0-9]+\.[0-9]+\.[0-9]+).*/\1/p')
     curlOptions=( -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15" )
     expectedTeamID="ACV7L3WCD8"
     ;;
