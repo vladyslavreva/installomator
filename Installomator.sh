093b1ada0226be4bf1b8363bc834701911119148
Vladyslav Reva <v.reva1997@gmail.com>
2025-09-24 11:12:49 +0300

    Change foxitpdfreader label and script version

--- Installomator.sh
+++ Installomator.sh
@@ -341,8 +341,8 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
         rosetta2=no
     fi
 fi
-VERSION="3.3"
-VERSIONDATE="2025-09-09"
+VERSION="3.4"
+VERSIONDATE="2025-09-24"
 
 # MARK: Functions
 
@@ -3436,7 +3436,15 @@ foxitpdfreader)
     name="Foxit PDF Reader"
     type="pkg"
     appNewVersion=$(curl -fsL "https://www.foxit.com/pdf-editor/version-history.html" | xmllint --html --xpath '//div[@id="tab-editor-suite-mac"]//h3/text()' - 2>/dev/null | head -n 1 | sed 's/Version //')
-    downloadURL="https://cdn01.foxitsoftware.com/pub/foxit/phantomPDF/desktop/mac/${appNewVersion%%.*}.x/${appNewVersion:0:6}/FoxitPDFReader${${appNewVersion:0:6}//./}.L10N.Setup.pkg"
+    majorNew="${appNewVersion%%.*}"
+    versionShort="${appNewVersion:0:6}"
+    pkgName="${versionShort//./}"
+    majorCurrent=$([[ -d "/Applications/${name}.app" ]] && defaults read "/Applications/${name}.app/Contents/Info.plist" CFBundleShortVersionString 2>/dev/null | cut -d. -f1)
+    if [[ "$majorCurrent" == "$majorNew" && -n "$majorCurrent" ]]; then
+        downloadURL="https://cdn01.foxitsoftware.com/pub/foxit/phantomPDF/desktop/mac/${majorNew}.x/${versionShort}/FoxitPDFReader${pkgName}Upd.L10N.pkg"
+    else
+        downloadURL="https://cdn01.foxitsoftware.com/pub/foxit/phantomPDF/desktop/mac/${majorNew}.x/${versionShort}/FoxitPDFReader${pkgName}.L10N.Setup.pkg"
+    fi
     expectedTeamID="8GN47HTP75"
     ;;
 franz)
